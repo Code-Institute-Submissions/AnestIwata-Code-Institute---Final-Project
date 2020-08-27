520195d5a60958175c737e6070e380a6e3b35762
Anestiwata <pawelpelech@gmail.com>
2020-08-27 07:10:48 +0200

    Added number of bugs and features to the landing page

--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from bugs.models import Bug
+from features.models import Feature
 
 
 def index(request):
-    return render(request, 'landing/index.html')
+    bugs = Bug.objects.all()
+    features = Feature.objects.all()
+    context = {"bugs": len(bugs),
+               "features": len(features)}
+    return render(request, 'landing/index.html', context)
